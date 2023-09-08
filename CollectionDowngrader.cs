edd60ec200d07744fabc85a1bb14899e68983ab3
kabii <jackyd.1018@live.com>
2023-09-08 11:43:59 -0500

    bump to schema version 34 (no changes needed)

--- CollectionDowngrader.cs
+++ CollectionDowngrader.cs
@@ -7,7 +7,7 @@ namespace CollectionDowngrader
 {
     class CollectionDowngrader
     {
-        const int LazerSchemaVersion = 32;
+        const int LazerSchemaVersion = 34;
 
         private static int Main(string[] args)
         {
